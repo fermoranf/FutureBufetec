f7ba15d0cd3cdc8134b3e886c30939017af98e80
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2024-09-19 12:14:58 -0600

    progress in backend

--- backEnd/controllers/citasController.js
+++ backEnd/controllers/citasController.js
@@ -2,8 +2,8 @@ const citas = require('../schemas/citasSchema');
 
 exports.getCitas = async (req, res) => {
     try {
-      const items = await citas.find();
-      res.status(200).json(items);
+      const citas = await citas.find();
+      res.status(200).json(citas);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
