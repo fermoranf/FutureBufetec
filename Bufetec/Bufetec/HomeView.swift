53fc2bc6ebd43422df6d5cc6521e9ceaa36069f3
Luis <a01722501@tec.mx>
2024-09-24 12:53:07 -0600

    front end

--- Bufetec/Bufetec/HomeView.swift
+++ Bufetec/Bufetec/HomeView.swift
@@ -11,30 +11,40 @@ import FirebaseAuth
 
 struct HomeView: View {
     
-    
+    @State private var navigateToHomePage = false
     @State private var userLoggedIn = (Auth.auth().currentUser != nil)
     var body: some View {
         VStack{
-            if(userLoggedIn){
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-
-            }else{
-                LoginView()
+            NavigationView{
+                if userLoggedIn {
+                    NavigationLink(
+                    destination: HomePageView(),isActive: $navigateToHomePage, // Automatically activates navigation
+                        label: {
+                            EmptyView() // No need for a visible label
+                            }
+                    )
+                    .onAppear {
+                        // Trigger the navigation as soon as the condition is met
+                    navigateToHomePage = true
+                }
             }
-                        
+                else{
+                    LoginView()
+                }
+                
+            }
         }
-        .onAppear(){
-            Auth.auth().addStateDidChangeListener{auth,user in
-                if(user != nil){
-                    userLoggedIn = true
-                }else{
-                    userLoggedIn=false
+            .onAppear(){
+                Auth.auth().addStateDidChangeListener{auth,user in
+                    if(user != nil){
+                        userLoggedIn = true
+                    }else{
+                        userLoggedIn=false
+                    }
                 }
             }
         }
     }
-}
-
-#Preview {
-    HomeView()
-}
+    #Preview {
+        HomeView()
+    }
