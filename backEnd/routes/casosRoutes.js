985e6b4ebc44107cd5c88a6367891de5ee425056
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2024-09-25 13:29:55 -0600

    casoid

--- backEnd/routes/casosRoutes.js
+++ backEnd/routes/casosRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/', casosController.getCasos);
 router.get('/:id', casosController.getCasoById);
-
+//http://localhost:3000/api/casos/66e9c3caa65dfe5e11969a35
 module.exports = router;
 
